7f44800523a2890c6ff203a0f7ef7fefa7255ba3
ZacGriffiths <48232543+ZacGriffiths@users.noreply.github.com>
2021-03-01 21:12:01 +1100

    Add files via upload

---
+++ Ass-01-PassingParameters.c
@@ -0,0 +1,18 @@
+/*        $Id: Ass-01-PassingParameters.c 4655 2021-02-14 07:29:57Z rbetz $
+ *  $Revision: 4655 $
+ *      $Date: 2021-02-14 18:29:57 +1100 (Sun, 14 Feb 2021) $
+ *    $Author: rbetz $
+ */
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+  int inI;
+  printf ("Number of arguments passed = %d\n", argc);
+  for (inI = 0; inI < argc; inI = inI + 1)
+  {
+    printf ("-> Argument number %d is %s\n",inI + 1, argv[inI]);
+  }
+  return 0;
+}
